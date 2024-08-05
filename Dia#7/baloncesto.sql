1a01cddf9416bd7b88573d4775c4e0a90a470e87
Maria Jose Bastilla Osorio <156432503+Majob2027@users.noreply.github.com>
2024-08-04 21:46:43 -0500

    Update baloncesto.sql

--- Dia#7/baloncesto.sql
+++ Dia#7/baloncesto.sql
@@ -4,25 +4,25 @@ create database baloncesto;
 use baloncesto;
 
 create table partido(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    equipoLocal varchar(225) not null,
-    equipoVisitante varchar(225) not null,
-    cestasLocal int not null,
-    cestasVisitante int not null,
+    id_partido int AUTO_INCREMENT PRIMARY KEY,
+    equipo_local varchar(225) not null,
+    equipo_visitante varchar(225) not null,
+    cestas_local int not null,
+    cestas_visitante int not null,
     estado enum("Activo", "Finalizado"),
-    fechaPartido date
+    fecha_partido date
 );
 
 create table liga (
-    id int AUTO_INCREMENT PRIMARY KEY,
+    id_partido_liga int AUTO_INCREMENT PRIMARY KEY,
     id_partido int,
-    Foreign Key (id_partido) REFERENCES partido(id),
+    Foreign Key (id_partido) REFERENCES partido(id_partido),
     jornada int
 );
 
 create table playOffs (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    id_partido int,
-    Foreign Key (id_partido) REFERENCES partido(id),
+    id_partido int AUTO_INCREMENT PRIMARY KEY,
+    id_partido_playoffs int,
+    Foreign Key (id_partido) REFERENCES partido(id_partido),
     ronda enum("Octavos", "Cuartos", "Final")
 );
