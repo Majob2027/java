ca19d46d5b69152ebd9cb96e6e9df6e5fe31ec85
Maria Jose Bastilla Osorio <156432503+Majob2027@users.noreply.github.com>
2024-08-05 09:03:46 -0500

    Add files via upload

---
+++ Dia#7/tablas.sql
@@ -0,0 +1,30 @@
+
+
+use bj0zivwbnxtzmbhrmehe;
+
+create table partido(
+    id_partido int AUTO_INCREMENT PRIMARY KEY,
+    equipo_local varchar(225) not null,
+    equipo_visitante varchar(225) not null,
+    cestas_local int not null,
+    cestas_visitante int not null,
+    estado enum("Activo", "Finalizado"),
+    fecha_partido date
+);
+
+create table liga (
+    id_partido_liga int AUTO_INCREMENT PRIMARY KEY,
+    id_partido int,
+    Foreign Key (id_partido) REFERENCES partido(id_partido),
+    jornada int
+);
+
+create table playOffs (
+    id_partido int AUTO_INCREMENT PRIMARY KEY,
+    id_partido_playoffs int,
+    Foreign Key (id_partido) REFERENCES partido(id_partido),
+    ronda enum("Octavos", "Cuartos", "Final")
+);
+
+select * from partido;
+select * from liga;
